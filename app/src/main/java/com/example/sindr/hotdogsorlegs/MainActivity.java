52e5e567e47158bb3dd56202065c554fe6c0d553
Sindreedelange <146169@stud.hvl.no>
2018-12-07 23:18:16 +0100

    Able to take a picture with the device's camera, and display it

--- app/src/main/java/com/example/sindr/hotdogsorlegs/MainActivity.java
+++ app/src/main/java/com/example/sindr/hotdogsorlegs/MainActivity.java
@@ -1,13 +1,102 @@
 package com.example.sindr.hotdogsorlegs;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_CODE = 1000;
+    private static final int IMAGE_CAPTURE_COD = 1001;
+
+    ImageView imageView;
+    Button btnCamera;
+
+    Uri image_uri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnCamera = findViewById(R.id.btnCamera);
+        imageView = findViewById(R.id.imageView);
+
+        // Button click
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    // if system os is >= marshmallow, request runtime permission
+                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
+                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ){
+                        // Permission not enabled, request it
+                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                        // Show popup to request permissions
+                        requestPermissions(permission, PERMISSION_CODE);
+                    } else {
+                        // Permission already granted
+                        openCamera();
+                    }
+                } else {
+                    // System os < marshmellow
+                }
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void openCamera() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.TITLE, "New Picture");
+        values.put(MediaStore.Images.Media.DESCRIPTION, "From the camera");
+        image_uri = getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+        // Camera intent
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri);
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_COD);
+    }
+
+    // Handling permission result
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        // Called when user presses Allow or deny from Permission Request Popup
+        switch (requestCode){
+            case PERMISSION_CODE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission granted
+                    openCamera();
+                } else {
+                    // Permission denied
+                    Toast.makeText(this, "Permision denied..", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // Called when image was captured from camera
+
+        if (resultCode == RESULT_OK){
+            // Set image captured to ImageView
+            imageView.setImageURI(image_uri);
+        }
     }
 }
