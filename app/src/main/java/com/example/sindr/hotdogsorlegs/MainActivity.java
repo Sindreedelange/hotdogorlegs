bbe567bc67772e815c2de3265d23fc30233993b4
Sindreedelange <146169@stud.hvl.no>
2018-12-08 18:46:01 +0100

    Added functionality for predicting on image. Still need an actual tf model

--- app/src/main/java/com/example/sindr/hotdogsorlegs/MainActivity.java
+++ app/src/main/java/com/example/sindr/hotdogsorlegs/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.sindr.hotdogsorlegs;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.media.Image;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -17,15 +20,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final int PERMISSION_CODE = 1000;
     private static final int IMAGE_CAPTURE_COD = 1001;
 
+    //Load the tensorflow inference library
+    static {
+        System.loadLibrary("tensorflow_inference");
+    }
+
+    //PATH TO OUR MODEL FILE AND NAMES OF THE INPUT AND OUTPUT NODES
+    private String MODEL_PATH = "file:///android_asset/squeezenet.pb";
+    private String INPUT_NAME = "input_1";
+    private String OUTPUT_NAME = "output_1";
+    private TensorFlowInferenceInterface tf;
+
+    //ARRAY TO HOLD THE PREDICTIONS AND FLOAT VALUES TO HOLD THE IMAGE DATA
+    float[] PREDICTIONS = new float[1000];
+    private float[] floatValues;
+    private int[] INPUT_SIZE = {224,224,3};
+
     ImageView imageView;
     Button btnCamera;
+    Button btnPredict;
+    TextView resultView;
+
 
     Uri image_uri;
 
@@ -35,29 +60,30 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnCamera = findViewById(R.id.btnCamera);
+        btnPredict = findViewById(R.id.predictImage);
         imageView = findViewById(R.id.imageView);
+
+        //initialize tensorflow with the AssetManager and the Model
+        tf = new TensorFlowInferenceInterface(getAssets(),MODEL_PATH);
 
         // Button click
         btnCamera.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                    // if system os is >= marshmallow, request runtime permission
-                    if (checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED ||
-                            checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED ){
-                        // Permission not enabled, request it
-                        String[] permission = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
-                        // Show popup to request permissions
-                        requestPermissions(permission, PERMISSION_CODE);
-                    } else {
-                        // Permission already granted
-                        openCamera();
-                    }
-                } else {
-                    // System os < marshmellow
-                }
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
                 startActivity(intent);
+            }
+        });
+
+        btnPredict.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    Bitmap image = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+                    predict(image);
+                } catch (Exception e){
+                    Toast.makeText(MainActivity.this, "Nope", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -98,5 +124,95 @@ public class MainActivity extends AppCompatActivity {
             // Set image captured to ImageView
             imageView.setImageURI(image_uri);
         }
+    }
+
+    //FUNCTION TO COMPUTE THE MAXIMUM PREDICTION AND ITS CONFIDENCE
+    public Object[] argmax(float[] array){
+
+
+        int best = -1;
+        float best_confidence = 0.0f;
+
+        for(int i = 0;i < array.length;i++){
+
+            float value = array[i];
+
+            if (value > best_confidence){
+
+                best_confidence = value;
+                best = i;
+            }
+        }
+
+        return new Object[]{best,best_confidence};
+    }
+
+    // Run prediction on an image
+    @SuppressLint("StaticFieldLeak")
+    public void predict(final Bitmap bitmap){
+
+
+        //Runs inference in background thread
+        new AsyncTask<Integer,Integer,Integer>(){
+
+            @Override
+
+            protected Integer doInBackground(Integer ...params){
+
+                //Resize the image into 224 x 224
+                Bitmap resized_image = ImageUtils.processBitmap(bitmap,224);
+
+                //Normalize the pixels
+                floatValues = ImageUtils.normalizeBitmap(resized_image,224,127.5f,1.0f);
+
+                //Pass input into the tensorflow
+                tf.feed(INPUT_NAME,floatValues,1,224,224,3);
+
+                //compute predictions
+                tf.run(new String[]{OUTPUT_NAME});
+
+                //copy the output into the PREDICTIONS array
+                tf.fetch(OUTPUT_NAME,PREDICTIONS);
+
+                //Obtained highest prediction
+                Object[] results = argmax(PREDICTIONS);
+
+
+                int class_index = (Integer) results[0];
+                float confidence = (Float) results[1];
+
+
+                try{
+
+                    final String conf = String.valueOf(confidence * 100).substring(0,5);
+
+                    //Convert predicted class index into actual label name
+                    final String label = ImageUtils.getLabel(getAssets().open("labels.json"),class_index);
+
+
+
+                    //Display result on UI
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            // progressBar.dismiss();
+                            resultView.setText(label + " : " + conf + "%");
+
+                        }
+                    });
+
+                }
+
+                catch (Exception e){
+
+                }
+                return 0;
+            }
+
+
+
+        }.execute(0);
+
     }
 }
